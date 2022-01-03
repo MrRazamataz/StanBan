972db0210f182713cc6a0afa71e2d72e62b0eaf3
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2022-01-03 15:48:04 +0000

    v1.0.0 coolness

--- src/main/java/raza/$stanban/PluginMain.java
+++ src/main/java/raza/$stanban/PluginMain.java
@@ -16,9 +16,24 @@ public class PluginMain extends JavaPlugin implements Listener {
 		instance = this;
 		getDataFolder().mkdir();
 		getServer().getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
 		try {
 			PluginMain.getInstance().getLogger().info(ChatColor.translateAlternateColorCodes('&',
 					"[&cStanBan&r] &aPlugin has been enabled and is active. Ready!"));
+			PluginMain.getInstance().getLogger()
+					.info(ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r] &6Running version: 1.0.0"));
+			PluginMain.getInstance().getLogger()
+					.info(ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r] Loading config..."));
+			PluginMain.getInstance().getLogger()
+					.info(ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r] Loading stan strings..."));
+			for (Object FINAL_loopValue1 : ((java.util.List) (Object) PluginMain.getInstance().getConfig()
+					.get("stanban.names"))) {
+				PluginMain.getInstance().getLogger().info(String.valueOf(FINAL_loopValue1));
+			}
+			PluginMain.getInstance().getLogger()
+					.info(ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r] Stan strings loaded!"));
+			PluginMain.getInstance().getLogger()
+					.info(ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r] Config loaded!"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -36,6 +51,20 @@ public class PluginMain extends JavaPlugin implements Listener {
 
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] commandArgs) {
+		if (command.getName().equalsIgnoreCase("stanbanreload")) {
+			try {
+				PluginMain.getInstance().reloadConfig();
+				((org.bukkit.entity.Player) (Object) commandSender)
+						.sendTitle(
+								ChatColor.translateAlternateColorCodes('&',
+										ChatColor.translateAlternateColorCodes('&',
+												"[&cStanBan&r] &aConfig reloaded!")),
+								((java.lang.String) null), ((int) (0d)), ((int) (100d)), ((int) (0d)));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
 		return true;
 	}
 
@@ -84,19 +113,28 @@ public class PluginMain extends JavaPlugin implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void event1(org.bukkit.event.player.PlayerJoinEvent event) throws Exception {
-		if ((((boolean) String.valueOf(((org.bukkit.entity.Player) event.getPlayer()))
-				.contains(String.valueOf("Dream")))
-				|| ((boolean) String.valueOf(((org.bukkit.entity.Player) event.getPlayer()))
-						.contains(String.valueOf("NotFound"))))) {
-			((org.bukkit.BanList) org.bukkit.Bukkit
-					.getBanList(((org.bukkit.BanList.Type) org.bukkit.BanList.Type.NAME))).addBan(
-							((java.lang.String) ((org.bukkit.command.CommandSender) (Object) ((org.bukkit.entity.Player) event
-									.getPlayer())).getName()),
-							ChatColor.translateAlternateColorCodes('&',
-									"&cYou have been detected as a Dream or GeorgeNotFound stan!"),
-							((java.util.Date) null), ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r]"));
-			org.bukkit.Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
-					"[&cStanBan&r] &a&lA stan was detected and was banned from the server!"));
+		Object $24c5b3a5b3df7260f1023d2b3f1dcdcc = null;
+		$24c5b3a5b3df7260f1023d2b3f1dcdcc = ((java.lang.Object) (Object) true);
+		for (Object FINAL_loopValue1 : ((java.util.List) (Object) PluginMain.getInstance().getConfig()
+				.get("stanban.names"))) {
+			if (((boolean) String.valueOf(((org.bukkit.entity.Player) event.getPlayer()))
+					.contains(String.valueOf(FINAL_loopValue1)))) {
+				((org.bukkit.BanList) org.bukkit.Bukkit
+						.getBanList(((org.bukkit.BanList.Type) org.bukkit.BanList.Type.NAME))).addBan(
+								((java.lang.String) ((org.bukkit.command.CommandSender) (Object) ((org.bukkit.entity.Player) event
+										.getPlayer())).getName()),
+								ChatColor.translateAlternateColorCodes('&',
+										String.valueOf(
+												PluginMain.getInstance().getConfig().get("stanban.ban-message"))),
+								((java.util.Date) null), ChatColor.translateAlternateColorCodes('&', "[&cStanBan&r]"));
+				((org.bukkit.entity.Player) event.getPlayer()).kickPlayer(ChatColor.translateAlternateColorCodes('&',
+						String.valueOf(PluginMain.getInstance().getConfig().get("stanban.ban-message"))));
+				if (((boolean) (Object) $24c5b3a5b3df7260f1023d2b3f1dcdcc)) {
+					org.bukkit.Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', String
+							.valueOf(PluginMain.getInstance().getConfig().get("stanban.chat-broadcast-message"))));
+					$24c5b3a5b3df7260f1023d2b3f1dcdcc = ((java.lang.Object) (Object) false);
+				}
+			}
 		}
 	}
 }
